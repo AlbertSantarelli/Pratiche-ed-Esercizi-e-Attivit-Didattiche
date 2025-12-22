e6176375925bc4aa2837a6f6e6e5c778503a75c3
Albert Santarelli <albertsantarelli2005@gmail.com>
2025-12-22 09:55:18 +0100

    Create Fondamenti di Python.py
    Esercitazione Pratica: Fondamenti di Python – Modulo 1
    Python From Zero to Hero – Master Python, AI & Machine Learning (Epicode)

---
+++ Fondamenti di Python.py
@@ -0,0 +1,15 @@
+print ("Hello World")
+
+Nome =  "By AlbertSantarelli"
+
+print (Nome)
+
+Nome = 23
+
+print (Nome)
+
+from colorama import init, Back, Fore, Style
+init(autoreset=True) #PrimoTeste.XD
+
+print(Back.WHITE + Fore.BLACK + "IL mio primo programma in python")
+print ("Xd")
